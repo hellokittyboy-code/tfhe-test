888023cb90e9e7801c8efb5386038e7009dbd7be
wubin <18519247208@qq.com>
2025-04-29 10:54:55 +0800

    add rust file

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode};
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::Relaxed;
 use chrono::Local;
-use log::info;
 
 fn main() -> Result<(), std::io::Error> {
+    println!("Test function executed");
     test_process();
     Ok(())
 }
 
 #[cfg(test)]
 fn main_test() {
-    info!("Test function executed");
+    println!("Test function executed");
     test_process();
 }
 
@@ -21,7 +21,7 @@ use tfhe::prelude::*;
 use tfhe::{generate_keys, set_server_key, ConfigBuilder, FheUint32, FheUint8, ClientKey, CompressedServerKey};
 fn test_process() -> Result<(), Box<dyn std::error::Error>> {
     let mut now = Local::now();
-    info!("[{}] start process init...", now.format("%Y-%m-%d %H:%M:%S%.3f"));
+    println!("[{}] start process init...", now.format("%Y-%m-%d %H:%M:%S%.3f"));
     // Basic configuration to use homomorphic integers
     //let config = ConfigBuilder::default().build();
 
@@ -48,28 +48,28 @@ fn test_process() -> Result<(), Box<dyn std::error::Error>> {
     set_server_key(server_keys);
 
     now = Local::now();
-    info!("[{}] set process server key...", now.format("%Y-%m-%d %H:%M:%S%.3f"));
+    println!("[{}] set process server key...", now.format("%Y-%m-%d %H:%M:%S%.3f"));
 
 
     // Clear equivalent computations: 1344 * 5 = 6720
     let encrypted_res_mul = &encrypted_a * &encrypted_b;
     let now = Local::now();
-    info!("[{}] set process sClear equivalent computations: 1344 * 5 = 6720...", now.format("%Y-%m-%d %H:%M:%S%.3f"));
+    println!("[{}] set process sClear equivalent computations: 1344 * 5 = 6720...", now.format("%Y-%m-%d %H:%M:%S%.3f"));
 
     // Clear equivalent computations: 6720 >> 5 = 210
     encrypted_a = &encrypted_res_mul >> &encrypted_b;
     let now = Local::now();
-    info!("[{}] set process Clear equivalent computations: 6720 >> 5 = 210", now.format("%Y-%m-%d %H:%M:%S%.3f"));
+    println!("[{}] set process Clear equivalent computations: 6720 >> 5 = 210", now.format("%Y-%m-%d %H:%M:%S%.3f"));
 
     // Clear equivalent computations: let casted_a = a as u8;
     let casted_a: FheUint8 = encrypted_a.cast_into();
     let now = Local::now();
-    info!("[{}] set process Clear equivalent computations: let casted_a = a as u8;", now.format("%Y-%m-%d %H:%M:%S%.3f"));
+    println!("[{}] set process Clear equivalent computations: let casted_a = a as u8;", now.format("%Y-%m-%d %H:%M:%S%.3f"));
 
     // Clear equivalent computations: min(210, 7) = 7
     let encrypted_res_min = &casted_a.min(&encrypted_c);
     let now = Local::now();
-    info!("[{}] Clear equivalent computations: min(210, 7) = 7", now.format("%Y-%m-%d %H:%M:%S%.3f"));
+    println!("[{}] Clear equivalent computations: min(210, 7) = 7", now.format("%Y-%m-%d %H:%M:%S%.3f"));
 
 
     // Operation between clear and encrypted data:
@@ -77,13 +77,13 @@ fn test_process() -> Result<(), Box<dyn std::error::Error>> {
     let encrypted_res = encrypted_res_min & 1_u8;
 
     let now = Local::now();
-    info!("[{}] encrpted res...", now.format("%Y-%m-%d %H:%M:%S%.3f"));
+    println!("[{}] encrpted res...", now.format("%Y-%m-%d %H:%M:%S%.3f"));
 
 
     // Decrypting on the client side:
     let clear_res: u8 = encrypted_res.decrypt(&client_key);
     assert_eq!(clear_res, 1_u8);
     let now = Local::now();
-    info!("[{}] test process executed successfully", now.format("%Y-%m-%d %H:%M:%S%.3f"));
+    println!("[{}] test process executed successfully", now.format("%Y-%m-%d %H:%M:%S%.3f"));
     Ok(())
 }
